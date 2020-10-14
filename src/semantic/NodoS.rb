4f18a601948b7a93ab540ce1a39e71432bed2f36
DinoMah <leithonpeas@hotmail.com>
2020-10-14 13:50:28 -0500

    Refactor for semantic node

--- src/semantic/NodoS.rb
+++ src/semantic/NodoS.rb
@@ -5,20 +5,11 @@ class NodoS
     attr_accessor :direccionMem
     attr_accessor :numsLinea
 
-    def initialize
-        @nombre = ""
-        @tipo = ""
-        @valor = 0
-        @direccionMem = ""
-        @numsLinea = ""
-    end
-
-    def initialize(nombre, tipo, valor, direccionMem, numsLinea)
-        @nombre = nombre
-        @tipo = tipo
-        @valor = valor
-        @direccionMem = direccionMem
-        @numsLinea = []
-        @numsLinea = numsLinea
+    def initialize(*args)
+        @nombre = args[0]
+        @tipo = args[1]
+        @valor = args[2]
+        @direccionMem = args[3]
+        @numsLinea = args[4]
     end
 end
